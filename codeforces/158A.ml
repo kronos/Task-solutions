192888f96be8a13b5b449b91921d119feec199a0
Ivan Samsonov <hronya@gmail.com>
2014-08-26 02:46:31 +0400

    codeforces 158A

---
+++ codeforces/158A.ml
@@ -0,0 +1,18 @@
+let read_int () = Scanf.scanf " %d" (fun x -> x)
+
+let solve n k =
+  let rec advanced_count n i current = match n with
+    | 0 -> i
+    | _ when current = 0 -> i
+    | _ -> let next = read_int () in
+            if current = next
+            then advanced_count (n - 1) (i + 1) next
+            else if i >= k || next = 0 then acc
+                 else advanced_count (n - 1) (i + 1) next
+  in advanced_count n 0 (-1)
+
+let () =
+  let n = read_int ()
+  and k = read_int ()
+  in Printf.printf "%d\n" (solve n k)
+
