02fa064e9f84ea627213b15770c6622d8e88866d
Ivan Samsonov <hronya@gmail.com>
2014-09-11 04:15:02 +0400

    Finish 1146 in Go

--- acm.timus.ru/1101-1200/1146.go
+++ acm.timus.ru/1101-1200/1146.go
@@ -3,48 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var n, max, t int
-	fmt.Scan(&n)
-	array := make([][]int, n)
-	sum := make([][]int, n)
+	var n, max, t, j int
+	fmt.Scanf("%d\n", &n)
+	array := make([][]int, n+1)
+	sum := make([][]int, n+1)
 
-	for i := 0; i < n; i++ {
-		array[i] = make([]int, n)
-		sum[i] = make([]int, n)
+	for i := 0; i <= n; i++ {
+		array[i] = make([]int, n+1)
+		sum[i] = make([]int, n+1)
 	}
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
+	for i := 1; i <= n; i++ {
+		for j = 1; j < n; j++ {
 			fmt.Scanf("%d", &array[i][j])
-			sum[i][j] = array[i][j]
-
-			if j > 0 {
-				sum[i][j] += sum[i][j-1]
-			}
-
-			if i > 0 {
-				sum[i][j] += sum[i-1][j]
-			}
-
-			if i > 0 && j > 0 {
-				sum[i][j] -= sum[i-1][j-1]
-			}
+			sum[i][j] = array[i][j] + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1]
 		}
+		fmt.Scanf("%d\n", &array[i][j])
+		sum[i][j] = array[i][j] + sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1]
 	}
-	max = array[0][0]
+	max = array[1][1]
 
-	for z := 0; z < n; z++ {
-		for k := 0; k < n; k++ {
-			for i := z; i < n; i++ {
-				for j := k; j < n; j++ {
-					t = sum[i][j]
-					if k < j {
-						t -= sum[i][k]
-					}
-
-					if z < i {
-						t -= sum[z][j]
-					}
+	for z := 1; z <= n; z++ {
+		for k := 1; k <= n; k++ {
+			for i := z; i <= n; i++ {
+				for j := k; j <= n; j++ {
+					t = sum[i][j] - sum[i][k-1] - sum[z-1][j] + sum[z-1][k-1]
 
 					if t > max {
 						max = t
